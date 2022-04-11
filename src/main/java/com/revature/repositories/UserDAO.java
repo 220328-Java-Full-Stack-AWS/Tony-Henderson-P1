08dc2aa3f1041b0ceff1786af38151a7a508acd4
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-04-11 16:48:56 -0500

    Implemented UserDAO Interface

--- src/main/java/com/revature/repositories/UserDAO.java
+++ src/main/java/com/revature/repositories/UserDAO.java
@@ -1,16 +1,33 @@
 package com.revature.repositories;
 
 import com.revature.models.User;
+import com.revature.interfaces.IUserDAO;
 
+import java.util.List;
 import java.util.Optional;
 
-public class UserDAO {
+public class UserDAO implements IUserDAO {
 
     /**
      * Should retrieve a User from the DB with the corresponding username or an empty optional if there is no match.
      */
     public Optional<User> getByUsername(String username) {
         return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> getById(int id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<User> getByEmail(String email) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<User> getAllUsers() {
+        return null;
     }
 
     /**
@@ -22,5 +39,15 @@ public class UserDAO {
      */
     public User create(User userToBeRegistered) {
         return userToBeRegistered;
+    }
+
+    @Override
+    public Optional<User> updateUserInfo(int id, User u) {
+        return Optional.empty();
+    }
+
+    @Override
+    public boolean deleteUser(User u) {
+        return false;
     }
 }
