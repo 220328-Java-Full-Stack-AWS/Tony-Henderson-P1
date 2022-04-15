9388f552866fac721d09e5e32a60bb06b72280e1
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-04-15 15:20:24 -0500

    Methods implement correct exceptions.

--- src/main/java/com/revature/repositories/IUserDAO.java
+++ src/main/java/com/revature/repositories/IUserDAO.java
@@ -1,5 +1,6 @@
 package com.revature.repositories;
 
+import com.revature.exceptions.*;
 import com.revature.models.User;
 
 import java.util.List;
@@ -11,7 +12,7 @@ public interface IUserDAO {
     Optional<User> getById(int id);
     Optional<User> getByEmail(String email);
     List<User> getAllUsers();
-    User create(User u);
-    Optional<User> updateUserInfo(int id, User u);
-    boolean deleteUser(User u);
+    User create(User u) throws RegistrationUnsuccessfulException, UserHasNonZeroIdException;
+    Optional<User> updateUserInfo(int id, User u) throws UpdateUserUnsuccessfulException;
+    boolean deleteUser(User u) throws DeleteUserUnsuccessfulException, CannotDeleteForeignKeyViolationException;
 }
