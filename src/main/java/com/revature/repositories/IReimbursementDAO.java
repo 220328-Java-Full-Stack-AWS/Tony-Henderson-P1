b3e405c7b8cc734c990cceb183b38788ac410c09
GH94GitHub <46788915+GH94GitHub@users.noreply.github.com>
2022-04-15 16:00:47 -0500

    Now defines proper exceptions to be thrown

--- src/main/java/com/revature/repositories/IReimbursementDAO.java
+++ src/main/java/com/revature/repositories/IReimbursementDAO.java
@@ -1,5 +1,9 @@
 package com.revature.repositories;
 
+import com.revature.exceptions.CreationUnsuccessfulException;
+import com.revature.exceptions.DeleteUnsuccessfulException;
+import com.revature.exceptions.ItemHasNonZeroIdException;
+import com.revature.exceptions.UpdateUnsuccessfulException;
 import com.revature.models.Reimbursement;
 import com.revature.models.Status;
 
@@ -9,7 +13,7 @@ import java.util.Optional;
 public interface IReimbursementDAO {
     Optional<Reimbursement> getById(int id);
     List<Reimbursement> getByStatus(Status status);
-    Reimbursement update(Reimbursement unprocessedReimbursement);
-    Reimbursement createRequest(Reimbursement newReimbursement);
-    boolean deleteRequest(int id);
+    Reimbursement update(Reimbursement unprocessedReimbursement) throws UpdateUnsuccessfulException;
+    Reimbursement createRequest(Reimbursement newReimbursement) throws CreationUnsuccessfulException, ItemHasNonZeroIdException;
+    boolean deleteRequest(int id) throws DeleteUnsuccessfulException;
 }
